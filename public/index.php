1d12c55ffe20aa5f107a78a1096e23d4a6908ea3
Rafael Bombonatti <bombonatti@gmail.com>
2024-06-04 10:49:24 -0300

    Reorganização das rotas

--- public/index.php
+++ public/index.php
@@ -1,21 +1,21 @@
 <?php
 
-require_once __DIR__ . '/../src/BalanceHandler.php';
-require_once __DIR__ . '/../src/EventHandler.php';
+require_once __DIR__ . '/../autoload.php';
 
-const ENDPOINT_BALANCE = '/balance';
-const ENDPOINT_EVENT = '/event';
+use App\Controllers\BalanceController;
+use App\Controllers\EventController;
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
-header('Content-Type: application/json');
+if ($uri === '/balance' && $method === 'GET') {
+    (new BalanceController())->getBalance();
 
-if ($uri === ENDPOINT_BALANCE && $method === 'GET') {
-    BalanceHandler::getBalance();
-} elseif ($uri === ENDPOINT_EVENT && $method === 'POST') {
-    EventHandler::handleEvent();
+} elseif ($uri === '/event' && $method === 'POST') {
+    (new EventController())->handleEvent();
+
 } else {
+    header('Content-Type: application/json');
     http_response_code(404);
     echo json_encode(['error' => 'Endpoint not found']);
 }
